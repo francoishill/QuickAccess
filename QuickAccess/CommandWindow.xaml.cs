73ebe4a11adfc8902b23c3e9ac105c55797fd2b0
francoishill <francoishill@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2011-10-31 13:42:42 +0000

    CommandWindow: Only marked todo item as DONE.
    git-svn-id: svn://localhost/quickaccess/trunk@80 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- QuickAccess/CommandWindow.xaml.cs
+++ QuickAccess/CommandWindow.xaml.cs
@@ -25,7 +25,7 @@ public partial class CommandWindow : Window
 		//TODO: Make these windows appear on different Threads
 		System.Windows.Forms.Application.EnableVisualStyles();
 		System.Windows.Forms.Integration.ElementHost.EnableModelessKeyboardInterop(this);
-		//TODO: Rather use one large window instead of 20 child windows
+		//DONE TODO: Rather use one large window instead of 20 child windows
 
 		this.Title = WindowTitle;
 		labelTitle.Content = WindowTitle.ToUpper();
