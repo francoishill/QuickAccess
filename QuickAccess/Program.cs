b18824d6f6a6321febcb0162c4f04cf6216c7abb
francoishill <francoishill@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2011-12-15 17:02:05 +0000

    Added a mutex to prevent multiple instances to be opened.
    git-svn-id: svn://localhost/quickaccess/trunk@115 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- QuickAccess/Program.cs
+++ QuickAccess/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace QuickAccess
@@ -13,9 +14,27 @@ namespace QuickAccess
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+					//QuickAccess-{6EBAC5AC-BCF2-4263-A82C-F189930AEA30}
+					//Mutex.
+
+					using (Mutex mutex = new Mutex(false, "QuickAccess-{6EBAC5AC-BCF2-4263-A82C-F189930AEA30}"))
+					{
+						if (!mutex.WaitOne(0, true))
+						{
+							Application.EnableVisualStyles();
+							MessageBox.Show("Another instances of QuickAccess is already running.", "Only one instance allowed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						}
+						else
+						{
+							Application.EnableVisualStyles();
+							Application.SetCompatibleTextRenderingDefault(false);
+							Application.Run(new Form1());
+						}
+					}
+
+						//Application.EnableVisualStyles();
+						//Application.SetCompatibleTextRenderingDefault(false);
+						//Application.Run(new Form1());
         }
     }
 }
