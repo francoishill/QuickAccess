6679f1e6243ddfcfb11184df8e3bb03c51018e6d
francoishill <francoishill@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2012-01-03 10:55:21 +0000

    Added some subcommands to TracXmlRpcCommand. Also refactored the TracXmlRpcInterop to use threading for all online calls, because we do not want the app to hang when they occur. 'Ticket #2
    git-svn-id: svn://localhost/quickaccess/trunk@134 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- TracXmlRpcPlugin/TracXmlRpcCommandPlugin.cs
+++ TracXmlRpcPlugin/TracXmlRpcCommandPlugin.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using InlineCommandToolkit;
 using InterfaceForQuickAccessPlugin;
+using SharedClasses;
 using OverrideToStringClass = InlineCommandToolkit.InlineCommands.OverrideToStringClass;
 
 namespace TracXmlRpcPlugin
@@ -17,10 +18,16 @@ namespace TracXmlRpcPlugin
 		public override string Description { get { return "Get information about Trac projects"; } }
 		public override string ArgumentsExample { get { return @"QuickAccess"; } }
 
+		private enum SubCommandsEnum { GetFieldLabels, GetTicketIDs, GetListOfMethods, ChangeLogs, GetFieldValuesOfTicket, GetAllTicketDescriptions }
+
+		private static List<string> subCommandList;
+		private static List<string> SubCommandList { get { if (subCommandList == null) subCommandList = EnumsInterop.GetStringListOfEnumNames(typeof(SubCommandsEnum)); return subCommandList; } }
+		private readonly static string[] projectNameList = new string[] { "QuickAccess", "MonitorSystem", "SharedClasses" };
+
 		private readonly ObservableCollection<string>[] predefinedArgumentsList =
 		{
-			new ObservableCollection<string>() { "getfieldlables", "getticketids", "getlistofmethods" },
-			new ObservableCollection<string>() { "QuickAccess", "MonitorSystem", "SharedClasses" }
+			new ObservableCollection<string>(SubCommandList),
+			new ObservableCollection<string>(projectNameList)
 		};
 		public override ObservableCollection<string>[] PredefinedArgumentsList { get { return predefinedArgumentsList; } }
 
@@ -34,8 +41,8 @@ namespace TracXmlRpcPlugin
 			errorMessage = "";
 			if (Index < argumentsReplaceKeyValuePair.Length && argumentsReplaceKeyValuePair[Index].ContainsKey(argumentValue))
 				argumentValue = argumentsReplaceKeyValuePair[Index][argumentValue];
-			if (Index >= 2)
-				errorMessage = "More than 2 arguments not allowed for Trac XmlRpc command (sub-command, project)";
+			if (Index >= 3)
+				errorMessage = "More than 3 arguments not allowed for Trac XmlRpc command (sub-command, project)";
 			else if (Index == 0 && string.IsNullOrWhiteSpace(argumentValue))
 				errorMessage = "First argument (sub-command) of Trac XmlRpc command may not be null/empty/whitespaces";
 			else if (Index == 0 && !(predefinedArgumentsList[0].ToArray()).Contains(argumentValue))
@@ -49,7 +56,9 @@ namespace TracXmlRpcPlugin
 		public override bool ValidateArguments(out string errorMessage, params string[] arguments)
 		{
 			errorMessage = "";
-			if (arguments.Length != 2) errorMessage = "Exactly 2 arguments required for Trac XmlRpc command (sub-command, project)";
+
+			if (arguments.Length < 2) errorMessage = "At least 2 arguments required for Trac XmlRpc command (sub-command, project, ticketid)";
+			else if (arguments.Length > 3) errorMessage = "More than 3 arguments not allowed for Trac XmlRpc command (sub-command, project, ticketid)";
 			else if (!PreValidateArgument(out errorMessage, 0, arguments[0]))
 				errorMessage = errorMessage + "";
 			else if (!PreValidateArgument(out errorMessage, 1, arguments[1]))
@@ -62,50 +71,91 @@ namespace TracXmlRpcPlugin
 		{
 			try
 			{
-				if (arguments[0] == "getfieldlables")
+				if (string.Equals(arguments[0], SubCommandsEnum.GetFieldLabels.ToString(), StringComparison.InvariantCultureIgnoreCase))
 				{
-					ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
-					{
-						List<string> tmplist = TracXmlRpcInterop.GetFieldLables(VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
-						int tmpcounter = 1;
-						foreach (string s in tmplist)
-							TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
-								this,
-								textFeedbackEvent,
-								"getfieldlables " + tmpcounter++ + ": " + s,
-								TextFeedbackType.Noteworthy);
-					},
-					ThreadName: "getfieldlabels");
+					//DONE: Warning, if the password userprompt pops up it crashes because the commands are run on separate threads
+					List<string> tmplist = TracXmlRpcInterop.GetFieldLables(VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
+					int tmpcounter = 1;
+					foreach (string s in tmplist)
+						TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
+							this,
+							textFeedbackEvent,
+							"getfieldlables " + tmpcounter++ + ": " + s,
+							TextFeedbackType.Noteworthy);
 				}
-				else if (arguments[0] == "getticketids")
+				else if (string.Equals(arguments[0], SubCommandsEnum.GetTicketIDs.ToString(), StringComparison.InvariantCultureIgnoreCase))
 				{
-					ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
-					{
-						int[] ids = TracXmlRpcInterop.GetTicketIds(VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
-						int tmpcounter = 1;
-						foreach (int i in ids)
-							TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
-								this,
-								textFeedbackEvent,
-								"getticketids " + tmpcounter++ + ": " + i.ToString(),
-								TextFeedbackType.Noteworthy);
-					},
-					ThreadName: "getticketids");
+					int[] ids = TracXmlRpcInterop.GetTicketIds(VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
+					int tmpcounter = 1;
+					foreach (int i in ids)
+						TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
+							this,
+							textFeedbackEvent,
+							"getticketids " + tmpcounter++ + ": " + i.ToString(),
+							TextFeedbackType.Noteworthy);
 				}
-				else if (arguments[0] == "getlistofmethods")
+				else if (string.Equals(arguments[0], SubCommandsEnum.GetListOfMethods.ToString(), StringComparison.InvariantCultureIgnoreCase))
 				{
-					ThreadingInterop.PerformVoidFunctionSeperateThread(() =>
+					string[] arr = TracXmlRpcInterop.GetListOfMethods(VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
+					int tmpcounter = 1;
+					foreach (string s in arr)
+						TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
+							this,
+							textFeedbackEvent,
+							"getlistofmethods " + tmpcounter++ + ": " + s.ToString(),
+							TextFeedbackType.Noteworthy);
+				}
+				else if (string.Equals(arguments[0], SubCommandsEnum.ChangeLogs.ToString(), StringComparison.InvariantCultureIgnoreCase))
+				{
+					int tmpticketid;
+					if (!int.TryParse(arguments[2], out tmpticketid))
 					{
-						string[] arr = TracXmlRpcInterop.GetListOfMethods(VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
+						errorMessage = "Argument 3 of Trac XmlRpc must be a valid integer.";
+						return false;
+					}
+					else
+					{
+						List<TracXmlRpcInterop.ChangeLogStruct> tmplist = TracXmlRpcInterop.ChangeLogs(tmpticketid, VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
 						int tmpcounter = 1;
-						foreach (string s in arr)
+						foreach (TracXmlRpcInterop.ChangeLogStruct cl in tmplist)
+							if (cl.Field == "comment" && !string.IsNullOrWhiteSpace(cl.NewValue))
+								TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
+									this,
+									textFeedbackEvent,
+									"changelogs " + tmpcounter++ + ": " + "Ticket #" + tmpticketid + ", " + cl.Field + ": " + cl.NewValue,
+									TextFeedbackType.Noteworthy);
+					}
+				}
+				else if (string.Equals(arguments[0], SubCommandsEnum.GetFieldValuesOfTicket.ToString(), StringComparison.InvariantCultureIgnoreCase))
+				{
+					int tmpticketid;
+					if (!int.TryParse(arguments[2], out tmpticketid))
+					{
+						errorMessage = "Argument 3 of Trac XmlRpc must be a valid integer.";
+						return false;
+					}
+					else
+					{
+						Dictionary<string, object> fieldvalues = TracXmlRpcInterop.GetFieldValuesOfTicket(tmpticketid, VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
+						int tmpcounter = 1;
+						foreach (string key in fieldvalues.Keys)
 							TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
 								this,
 								textFeedbackEvent,
-								"getlistofmethods " + tmpcounter++ + ": " + s.ToString(),
+								"getfieldvaluesofticket " + tmpcounter++ + ": " + "Ticket #" + tmpticketid + ", " + key + ": " + fieldvalues[key].ToString(),
 								TextFeedbackType.Noteworthy);
-					},
-					ThreadName: "getlistofmethods");
+					}
+				}
+				else if (string.Equals(arguments[0], SubCommandsEnum.GetAllTicketDescriptions.ToString(), StringComparison.InvariantCultureIgnoreCase))
+				{
+					Dictionary<int, string> ticketDescriptions = TracXmlRpcInterop.GetAllTicketDescriptions(VisualStudioInterop.GetTracXmlRpcHttpPathFromProjectName(arguments[1]));
+					int tmpcounter = 1;
+					foreach (int i in ticketDescriptions.Keys)
+						TextFeedbackEventArgs.RaiseTextFeedbackEvent_Ifnotnull(
+							this,
+							textFeedbackEvent,
+							"getallticketdescriptions " + tmpcounter++ + ", #" + i + ": " + ticketDescriptions[i],
+							TextFeedbackType.Noteworthy);
 				}
 				else
 				{
@@ -124,11 +174,24 @@ namespace TracXmlRpcPlugin
 
 		private CommandArgument[] availableArguments = new CommandArgument[]
 			{
-				new CommandArgument("", "sub-command", new ObservableCollection<string>() { "getfieldlables", "getticketids", "getlistofmethods" }),
-				new CommandArgument("", "project name", new ObservableCollection<string>() { "QuickAccess", "MonitorSystem", "SharedClasses" })
+				new CommandArgument("", "sub-command", new ObservableCollection<string>(SubCommandList)),
+				new CommandArgument("", "project name", new ObservableCollection<string>(projectNameList)),
+				new CommandArgument("", "ticket id", new ObservableCollection<string>())
 			};
 		public override CommandArgument[] AvailableArguments { get { return availableArguments; } set { availableArguments = value; } }
 
-		public override int ArgumentCountForCurrentPopulatedArguments { get { return 2; } }
+		public override int ArgumentCountForCurrentPopulatedArguments
+		{
+			get
+			{
+				if (currentArguments.Count > 0 && (new string[] 
+					{
+						SubCommandsEnum.ChangeLogs.ToString().ToLower(),
+						SubCommandsEnum.GetFieldValuesOfTicket.ToString().ToLower() 
+					}).Contains(currentArguments[0].CurrentValue.ToLower()))
+					return 3;
+				else return 2;
+			}
+		}
 	}
 }
