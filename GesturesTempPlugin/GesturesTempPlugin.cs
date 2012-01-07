ee38144643f8c86bdba0951c33c2ef9d92df1a71
francoishill <francoishill@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2012-01-07 02:07:01 +0000

    Refactored and renamed IMouseGestures to IMouseGesture. Also added some test plugins inside the same file GesturesTempPlugin.cs.
    git-svn-id: svn://localhost/quickaccess/trunk@149 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- GesturesTempPlugin/GesturesTempPlugin.cs
+++ GesturesTempPlugin/GesturesTempPlugin.cs
@@ -4,20 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InterfaceForQuickAccessPlugin;
+using SharedClasses;
 
 namespace MouseGesturePlugins
 {
-	public class GesturesTemp : IMouseGestures, IQuickAccessPluginInterface
+	public class GesturesTemp1 : IMouseGesture, IQuickAccessPluginInterface
 	{
 		public string GestureString { get { return "UR"; } }
 
-		public string ThisCommandName { get { return "Temp"; } }
+		public string ThisCommandName { get { return "Temp1"; } }
 
 		public bool PerformCommandAfterGesture(out string ErrorMessage)
 		{
 			try
 			{
 				System.Diagnostics.Process.Start(@"c:\francois");
+				ErrorMessage = null;
+				return true;
+			}
+			catch (Exception exc)
+			{
+				ErrorMessage = exc.Message;
+				return false;
+			}
+		}
+	}
+
+	public class GesturesTemp2 : IMouseGesture, IQuickAccessPluginInterface
+	{
+		public string GestureString { get { return "UL"; } }
+
+		public string ThisCommandName { get { return "Temp2"; } }
+
+		public bool PerformCommandAfterGesture(out string ErrorMessage)
+		{
+			try
+			{
+				System.Diagnostics.Process.Start(@"c:\windows");
+				ErrorMessage = null;
+				return true;
+			}
+			catch (Exception exc)
+			{
+				ErrorMessage = exc.Message;
+				return false;
+			}
+		}
+	}
+
+	public class GesturesTemp3 : IMouseGesture, IQuickAccessPluginInterface
+	{
+		public string GestureString { get { return "DR"; } }
+
+		public string ThisCommandName { get { return "Temp3"; } }
+
+		public bool PerformCommandAfterGesture(out string ErrorMessage)
+		{
+			try
+			{
+				string clipboardText = "francoishill11@gmail.com";
+				int durationTextWillBeInClipboard = 5000;
+				UserMessages.ShowMessage("Press ok and then the clipboard will be set to \"" + clipboardText + "\" and removed after " + durationTextWillBeInClipboard + " milliseconds");
+				System.Windows.Forms.Clipboard.SetText(clipboardText);
+				OverlayGesturesForm.RunMethodAfterMilliseconds(delegate { System.Windows.Forms.Clipboard.Clear(); }, durationTextWillBeInClipboard);
 				ErrorMessage = null;
 				return true;
 			}
--- InlineCommandToolkit/MouseGestures.cs
+++ InlineCommandToolkit/MouseGestures.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MouseGesturePlugins
 {
-	public interface IMouseGestures
+	public interface IMouseGesture
 	{
 		string GestureString { get; }
 		string ThisCommandName { get; }
