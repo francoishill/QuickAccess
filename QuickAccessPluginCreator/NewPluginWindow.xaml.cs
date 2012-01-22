70ec57d8de20ff37641a556bbf562d4b12d061e2
francoishill <francoishill@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2012-01-22 22:36:37 +0000

    refs #1: Can now create a new command plugin, the relevant files will be generated and the csproj will be built. This resulting dll (in bin\Release) can be copied to ProgramFiles\QuickAccess\Plugins and it should work.
    git-svn-id: svn://localhost/quickaccess/trunk@175 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- QuickAccessPluginCreator/NewPluginWindow.xaml.cs
+++ QuickAccessPluginCreator/NewPluginWindow.xaml.cs
@@ -33,40 +33,154 @@ namespace QuickAccessPluginCreator
 			tmp();
 		}
 
-		private const string TemplateInitialPart = "QuickAccessPluginCreator.Templates";
+		private const string TemplateInitialPart = "QuickAccessPluginCreator.Templates.";
 		private void tmp()
 		{
+			string newCommandName = UserMessages.Prompt("Please enter the command name");
+
+			if (newCommandName == null)
+				return;
+
 			Dictionary<string, List<string>> templates = new Dictionary<string, List<string>>(StringComparer.InvariantCultureIgnoreCase);
 
-			string tempDir = Path.GetTempPath() + "NewPlugins";
+			string tempDir = Path.GetTempPath() + "NewPlugins\\";
 
 			System.Reflection.Assembly objAssembly = System.Reflection.Assembly.GetExecutingAssembly();
 			string[] myResources = objAssembly.GetManifestResourceNames();
 			foreach (string s in myResources)
 				if (s.ToLower().StartsWith(TemplateInitialPart.ToLower()))
 				{
-					string pathWithoutInitialPart = s.Substring(TemplateInitialPart.Length + 1);
+					string pathWithoutInitialPart = s.Substring(TemplateInitialPart.Length);
 					string folderName = pathWithoutInitialPart.Split('.')[0];
 					if (!templates.ContainsKey(folderName))
 						templates.Add(folderName, new List<string>());
 					templates[folderName].Add(s);
 				}
 
-			//foreach (string key in templates.Keys)
-			//{
-			//	foreach (string file in templates[key])
-			//	{
-			//		Stream stream = objAssembly.GetManifestResourceStream(file);
-			//		int length = (int)stream.Length;
-			//		byte[] bytesOfPublishHtmlTemplateDLL = new byte[length];
-			//		stream.Read(bytesOfPublishHtmlTemplateDLL, 0, length);
-			//		stream.Close();
-			//		FileStream fileStream = new FileStream(tempFilename, FileMode.Create);
-			//		fileStream.Write(bytesOfPublishHtmlTemplateDLL, 0, length);
-			//		fileStream.Close();
-			//		string textOfFile = File.ReadAllText(tempFilename);
-			//	}
-			//}
+			foreach (string key in templates.Keys)
+			{
+				foreach (string file in templates[key])
+				{
+					string fileRelativePath = file.Substring(TemplateInitialPart.Length);
+					int lastDotPos = fileRelativePath.LastIndexOf('.');
+					fileRelativePath = fileRelativePath.Replace('.', '\\');
+					string fileRelativeLastDotPutBackAgain =
+						fileRelativePath.Substring(0, lastDotPos) +
+						"." +
+						fileRelativePath.Substring(lastDotPos + 1);
+
+					string fullpathOfFile = tempDir + fileRelativeLastDotPutBackAgain;
+					string fileDir = Path.GetDirectoryName(fullpathOfFile);
+					if (Directory.Exists(fileDir))
+					{
+						try
+						{
+							Array.ForEach(Directory.GetFiles(fileDir, "*.*", SearchOption.AllDirectories), path => File.Delete(path));
+							Directory.Delete(fileDir, true);
+						}
+						catch (Exception exc)
+						{
+							UserMessages.ShowWarningMessage(exc.Message);
+						}
+					}
+				}
+			}
+
+			foreach (string key in templates.Keys)
+			{
+				string tmpCsProjFilepath = null;
+				//0				  10				20				30				40				50				60				70				80
+				//01234567890123456789012345678901234567890123456789012345678901234567890123456789012
+				//QuickAccessPluginCreator.Templates.CommandPluginTemplate.Properties.AssemblyInfo.cs
+				foreach (string file in templates[key])
+				{
+					string fileRelativePath = file.Substring(TemplateInitialPart.Length);//CommandPluginTemplate.Properties.AssemblyInfo.cs
+					int lastDotPos = fileRelativePath.LastIndexOf('.');//80
+					fileRelativePath = fileRelativePath.Replace('.', '\\');
+					string fileRelativeLastDotPutBackAgain =
+						fileRelativePath.Substring(0, lastDotPos) +
+						"." +
+						fileRelativePath.Substring(lastDotPos + 1);
+
+					string fullpathOfFile = tempDir + fileRelativeLastDotPutBackAgain;
+					string fileDir = Path.GetDirectoryName(fullpathOfFile);
+					//Directories are deleted in the previous loop (to ensure only the new files are there, no old ones)
+					Directory.CreateDirectory(Path.GetDirectoryName(fullpathOfFile));
+
+					Stream stream = objAssembly.GetManifestResourceStream(file);
+					int length = (int)stream.Length;
+					byte[] bytesOfPublishHtmlTemplateDLL = new byte[length];
+					stream.Read(bytesOfPublishHtmlTemplateDLL, 0, length);
+					stream.Close();
+
+					fullpathOfFile = fullpathOfFile.Replace("NewCommandPluginTemplate", newCommandName);
+
+					FileStream fileStream = new FileStream(fullpathOfFile, FileMode.Create);
+					fileStream.Write(bytesOfPublishHtmlTemplateDLL, 0, length);
+					fileStream.Close();
+					//string textOfFile = File.ReadAllText(fullpathOfFile);
+					//textOfFile = textOfFile.Replace("NewCommandPluginTemplate", newCommandName);
+
+					string[] lines = File.ReadAllLines(fullpathOfFile);
+					List<string> newLines = new List<string>();
+					Dictionary<string, string> fullReplacementTokens = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+					foreach (string lin in lines)
+					{
+						string line = lin.Replace("NewCommandPluginTemplate", newCommandName);
+						newLines.Add(line);
+
+						if (line.Contains("[{") && line.Contains("}]"))
+						{
+							string tmpline = line;
+							while (tmpline.Contains("[{") && tmpline.Contains("}]"))
+							{
+								int startIndex = tmpline.IndexOf("[{");
+								int endIndex = tmpline.IndexOf("}]");
+								string fullReplacementToken = tmpline.Substring(startIndex, endIndex + 2 - startIndex);
+								if (!fullReplacementTokens.ContainsKey(fullReplacementToken))
+									fullReplacementTokens.Add(fullReplacementToken, null);
+								tmpline = tmpline.Substring(endIndex + 2);
+							}
+						}
+					}
+
+					List<string> keyList = fullReplacementTokens.Keys.ToList();
+					for (int i = 0; i < keyList.Count; i++)
+					{
+						string keyreplace = keyList[i];
+						string answer = null;
+						if (keyreplace.Substring(2, keyreplace.Length - 4).ToLower() == "CommandName".ToLower())
+							answer = newCommandName;
+						else
+							answer = UserMessages.Prompt("Please enter " + keyreplace.Substring(2, keyreplace.Length - 4));
+						if (answer == null)
+							continue;
+						fullReplacementTokens[keyreplace] = answer;
+					}
+
+					for (int i = 0; i < newLines.Count; i++)
+						foreach (string replacement in fullReplacementTokens.Keys)
+							newLines[i] = newLines[i].Replace(replacement, fullReplacementTokens[replacement]);
+
+					File.WriteAllLines(fullpathOfFile, newLines);
+					if (fullpathOfFile.ToLower().EndsWith(".csproj"))
+						tmpCsProjFilepath = fullpathOfFile;
+				}
+				if (tmpCsProjFilepath != null && File.Exists(tmpCsProjFilepath))
+				{
+					VisualStudioInterop.BuildVsProjectReturnNewversionString(
+						newCommandName,
+						tmpCsProjFilepath,
+						"",
+						false,
+						VisualStudioInterop.BuildType.Rebuild,
+						VisualStudioInterop.ProjectConfiguration.Release,
+						VisualStudioInterop.PlatformTarget.x86,
+						false,
+						null);
+					System.Diagnostics.Process.Start("explorer", "/select, " + tmpCsProjFilepath);
+				}
+			}
 
 			//UserMessages.ShowInfoMessage(templates.ToString());
 			//foreach (string reso in myResources)
--- QuickAccessPluginCreator/Templates/CommandPluginTemplate/NewCommandPluginTemplate.cs
+++ QuickAccessPluginCreator/Templates/CommandPluginTemplate/NewCommandPluginTemplate.cs
@@ -1,63 +1,82 @@
-﻿using System;
+﻿using System.Collections.ObjectModel;
 using InlineCommandToolkit;
 
-namespace NewCommandPluginTemplate
+namespace [{CommandName}]CommandPlugin
 {
-	public class NewCommandPluginTemplate : InlineCommands.OverrideToStringClass, IQuickAccessPluginInterface
+	public class [{CommandName}]CommandPlugin : InlineCommands.OverrideToStringClass, IQuickAccessPluginInterface
 	{
 		//The command name must be a name without spaced (preferably all small caps).
-		public override string CommandName { get { throw new NotImplementedException(); } }
+		public override string CommandName { get { return "[{CommandName}]"; } }
 
 		//The display name may be a more "human friendly" name
-		public override string DisplayName { get { throw new NotImplementedException(); } }
+		public override string DisplayName { get { return "[{DisplayName}]"; } }
 
 		//A short description of what the command does
-		public override string Description { get { throw new NotImplementedException(); } }
+		public override string Description { get { return "[{Description}]"; } }
 
 		//An example of arguments
-		public override string ArgumentsExample { get { throw new NotImplementedException(); } }
+		public override string ArgumentsExample { get { return "[{ArgumentsExample}]"; } }
 
 		//This method is the validation that happens before a specific argument is "accepted"
 		public override bool PreValidateArgument(out string errorMessage, int Index, string argumentValue)
 		{
-			throw new NotImplementedException();
+			//TODO: This method must validate a specific argument
+			errorMessage = "";
+			return true;
 		}
 
 		//This method is the method that validate ALL arguments
 		public override bool ValidateArguments(out string errorMessage, params string[] arguments)
 		{
-			throw new NotImplementedException();
+			//TODO: This method must validate an array of arguments
+			errorMessage = "";
+			return true;
 		}
 
 		//This is where the command actually get performed (after successful validation)
 		public override bool PerformCommand(out string errorMessage, TextFeedbackEventHandler textFeedbackEvent = null, ProgressChangedEventHandler progressChangedEvent = null, params string[] arguments)
 		{
-			throw new NotImplementedException();
+			//TODO: Perform the command here
+			errorMessage = "";
+			UserMessages.ShowMessage("This is where the command is performed");
+			return true;
 		}
 
 		//This lists all the available arguments (with their "auto-complete" list). The ArgumentCountFor..(later defined) will determine which of these available arguments are shown
+		private CommandArgument[] availableArguments = new CommandArgument[]
+		{
+			new CommandArgument("", "temp", new ObservableCollection<string>() { "runnow" })
+		};
 		public override CommandArgument[] AvailableArguments
 		{
-			get { throw new NotImplementedException(); }
-			set { throw new NotImplementedException(); }
+			get { return availableArguments; }
+			set { availableArguments = value; }
 		}
+
+		private readonly ObservableCollection<string>[] predefinedArgumentsList =
+		{
+			//TODO: This must return the "auto-complete" list for the arguments
+			new ObservableCollection<string>() { "runnow" }
+		};
 
 		//This is an array of lists to define the "autocomplete" strings
-		public override System.Collections.ObjectModel.ObservableCollection<string>[] PredefinedArgumentsList
-		{
-			get { throw new NotImplementedException(); }
-		}
+		public override System.Collections.ObjectModel.ObservableCollection<string>[] PredefinedArgumentsList { get { return predefinedArgumentsList; } }
 
-		//This is an array of lists to define replacements for each argument
-		public override System.Collections.Generic.Dictionary<string, string>[] ArgumentsReplaceKeyValuePair
+		private System.Collections.Generic.Dictionary<string, string>[] argumentsReplaceKeyValuePair =
 		{
-			get { throw new NotImplementedException(); }
-		}
+			//TODO: This must return the replace values like if progfiles must be replaced with "Program Files" for a specific argument
+		};
+		//This is an array of lists to define replacements for each argument
+		public override System.Collections.Generic.Dictionary<string, string>[] ArgumentsReplaceKeyValuePair { get { return argumentsReplaceKeyValuePair; } }
 
 		//This must return the argument count visible to the user (may vary depending what is already in the currentArguments).
 		public override int ArgumentCountForCurrentPopulatedArguments
 		{
-			get { throw new NotImplementedException(); }
+			get
+			{
+				//TODO: The number of "visible" arguments, a statement can be used to determine this
+				return 1;
+			}
 		}
 	}
 }
