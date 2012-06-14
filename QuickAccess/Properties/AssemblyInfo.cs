d1a903b1a2cd3668191f9d4fb5b2cad699eb0945
francoishill <francoishill@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2012-06-14 14:16:43 +0000

    refs #1: Copied version number into AssemblyInfo.cs.
    git-svn-id: svn://localhost/quickaccess/trunk@210 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- QuickAccess/Properties/AssemblyInfo.cs
+++ QuickAccess/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -37,5 +37,5 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.133")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
