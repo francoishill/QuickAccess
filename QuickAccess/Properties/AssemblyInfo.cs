f2d8fe368cba1f782df8121a3682240120901e4d
francoishill <francoishill@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2012-07-12 06:46:07 +0000

    refs #1: Updated revision only.
    git-svn-id: svn://localhost/quickaccess/trunk@217 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- QuickAccess/Properties/AssemblyInfo.cs
+++ QuickAccess/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.173")]
+[assembly: AssemblyFileVersion("1.0.0.174")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
