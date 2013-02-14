de316d7d8d024036b64c13d6eb94b4b9856ced22
francois <francois@a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61>
2013-02-14 11:53:58 +0000

    Just some refactoring to get it working again.
    git-svn-id: svn://localhost/quickaccess/trunk@260 a15eb0d2-3cad-0410-ba7c-c0f4daa0cd61

--- QuickAccess/Properties/AssemblyInfo.cs
+++ QuickAccess/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.237")]
+[assembly: AssemblyFileVersion("1.0.0.242")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
