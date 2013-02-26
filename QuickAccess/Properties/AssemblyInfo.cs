edc35986a3b1dd6e2ac7167a5f8d7928cbcc2ab3
Francois Hill <francoishill11@gmail.com>
2013-02-26 18:31:29 +0200

    Deleted Pdf2TextPlugin, references DLLs which are larger than 6MB.

--- QuickAccess/Properties/AssemblyInfo.cs
+++ QuickAccess/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.244")]
+[assembly: AssemblyFileVersion("1.0.0.247")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
